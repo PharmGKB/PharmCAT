2ebb200e2f22514cb3715b722f2633cf3d0cb7db
Mark Woon <markwoon@gmail.com>
2016-04-12 14:53:10 -0700

    Make Haplotype and Variant Comparable.

--- src/main/java/org/cpic/haplotype/Haplotype.java
+++ src/main/java/org/cpic/haplotype/Haplotype.java
@@ -1,17 +1,30 @@
 package org.cpic.haplotype;
 
-public class Haplotype {
-	
+import org.apache.commons.lang3.ObjectUtils;
+import org.cpic.util.HaplotypeNameComparator;
+
+public class Haplotype implements Comparable<Haplotype> {
+
 	Variant varaints[];
 	String AlleleID;
 	String CommonName;
 	String FunctionStatus;
-	String NormalAlleles[]; 
+	String NormalAlleles[];
 	String EffectAlleles[];
-	
-			
-	
-	
-	
 
+
+  @Override
+  public String toString() {
+    return CommonName;
+  }
+
+
+  @Override
+  public int compareTo(Haplotype o) {
+    int rez = HaplotypeNameComparator.getComparator().compare(CommonName, o.CommonName);
+    if (rez != 0) {
+      return rez;
+    }
+    return ObjectUtils.compare(AlleleID, o.AlleleID);
+  }
 }
