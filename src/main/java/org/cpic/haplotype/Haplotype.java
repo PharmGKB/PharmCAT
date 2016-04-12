4e46afab67601ce31fd6aff576c6d1f0dcfb7d00
nate <nate@nate-Latitude-E6510>
2016-04-12 13:17:54 -0700

    New Haplotype Object

---
+++ src/main/java/org/cpic/haplotype/Haplotype.java
@@ -0,0 +1,17 @@
+package org.cpic.haplotype;
+
+public class Haplotype {
+	
+	Variant varaints[];
+	String AlleleID;
+	String CommonName;
+	String FunctionStatus;
+	String NormalAlleles[]; 
+	String EffectAlleles[];
+	
+			
+	
+	
+	
+
+}
