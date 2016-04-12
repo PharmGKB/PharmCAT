f0fa0d2750354caae0f0eac21f4ea0d2880ca20f
Mark Woon <markwoon@gmail.com>
2016-04-12 14:36:28 -0700

    Fix logging compile problem.

--- src/main/java/org/cpic/reporter/Reporter.java
+++ src/main/java/org/cpic/reporter/Reporter.java
@@ -1,57 +1,53 @@
 package org.cpic.reporter;
 
+import org.apache.commons.cli.*;
+import org.apache.commons.io.FileUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import java.io.File;
 import java.io.IOException;
 import java.util.Properties;
 
-import org.apache.commons.cli.CommandLine;
-import org.apache.commons.cli.DefaultParser;
-import org.apache.commons.cli.HelpFormatter;
-import org.apache.commons.cli.Option;
-import org.apache.commons.cli.Options;
-import org.apache.commons.io.FileUtils;
-import org.apache.commons.logging.Log;
-import org.apache.commons.logging.LogFactory;
-
 public class Reporter {
-    
+
     /**
      * Logging instance
      */
-   private Log logger = LogFactory.getLog( Reporter.class );
-    
+   private Logger logger = LoggerFactory.getLogger( Reporter.class );
+
     /**
      * Configuration properties loaded from file
      */
     private Properties props;
-    
+
     /**
      * File in
      */
     private File inFile;
-    
+
     /**
      * File to write final results to
      */
     private File outFile;
-    
 
-    
+
+
     //parse command line options
     private Reporter( CommandLine cmdline )  throws IOException {
-        
+
         File propsFile = new File( cmdline.getOptionValue( "conf" ) );
         props = readConfFile( propsFile );
-        
+
         String out_location = cmdline.getOptionValue( "outFile" );
         this.outFile = new File( out_location );
-        
+
         String in_file = cmdline.getOptionValue("inFile");
         this.inFile = new File( in_file );
-       
-                                
+
+
     }
-    
+
     /**
      * Read config file into props object
      */
@@ -63,24 +59,24 @@ public class Reporter {
 
     public static Options createCommandLineOptions() {
         Options options = new Options();
-       
+
         Option infile = new Option("inFile", true, "required - haplotype result to generate report");
         options.addOption(infile);
-        
+
         Option conf = new Option( "conf", true, "required - configuration file" );
         options.addOption( conf );
-        
-        
+
+
         Option outfile = new Option( "outFile", true, "required - location to write final file" );
         options.addOption( outfile );
-       
+
         return options;
     }
 
     public static void main(String[] args) throws Exception {
         Options options = createCommandLineOptions();
         CommandLine cmdline = new DefaultParser().parse( options,  args );
-        
+
         if( args.length == 0 ) {
             HelpFormatter formatter = new HelpFormatter();
             formatter.printHelp( "CPIC-Reporter", options );
@@ -93,26 +89,26 @@ public class Reporter {
             System.out.println( "Error in options in, out, conf" +
                     " are required variables" );
             System.exit( 1 );
-        
+
         } else if ( !cmdline.hasOption("inputBam")){
             System.out.println( "WARNING: No bam file provided" );
         }
-        
+
         //if minimal required parameters are set parse command line
         Reporter report = new Reporter( cmdline );
         //run genotyper workflow
         report.run();
     }
-    
+
     private void run() throws IOException {
-  
-        
+
+
         logger.info( "Complete" );
     }
 
-    
-    
-    
-  
+
+
+
+
 
 }
