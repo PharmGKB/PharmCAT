b89fa1eedc0d5a6cfd860740dab4d98aa921ac8d
nate <nate@nate-Latitude-E6510>
2016-04-12 14:56:45 -0700

    updates

--- src/main/java/org/cpic/haplotype/Variant.java
+++ src/main/java/org/cpic/haplotype/Variant.java
@@ -1,16 +1,84 @@
 package org.cpic.haplotype;
 
+import java.util.ArrayList;
+
 import org.apache.commons.lang3.ObjectUtils;
 
 public class Variant implements Comparable<Variant> {
-	String CHROM;
-	int POS;
-	String cDNA;
-	String ProteingEffect;
-	String [] ALTs;
-	String REF;
-	String rsID;
-	String GENE;
+	
+	private String CHROM;
+	private String GeneName;
+	private int POS;
+	private int GenePOS;
+	private ArrayList<String> cDNA;
+	private ArrayList<String> ProteingEffect;
+	private ArrayList<String> ALTs;
+	private String REF;
+	private String rsID;
+	
+	
+	public Variant(){
+	}
+	public Variant(String _CHROM, String _GeneName,String _cDNA){
+		CHROM = _CHROM;
+		GeneName = _GeneName;
+		if (_cDNA.contains(";")){
+			String [] fields = _cDNA.split(";");
+			for (int i = 0; i < fields.length; i++){
+				cDNA.add(fields[i].trim());
+			}
+		}
+		cDNA.add(_cDNA);
+	}
+	public String getCHROM(){
+		return CHROM;
+	}
+	public String getGeneName(){
+		return GeneName;
+	}
+	public void set_cDNA(String _cDNA){
+		if (_cDNA.contains(";")){
+			String [] fields = _cDNA.split(";");
+			for (int i = 0; i < fields.length; i++){
+				cDNA.add(fields[i].trim());
+			}
+		}
+		cDNA.add(_cDNA);
+	}
+	public void setProteingEffect(String _ProteingEffect){
+		if (_ProteingEffect.contains(";")){
+			String [] fields = _ProteingEffect.split(";");
+			for (int i = 0; i < fields.length; i++){
+				ProteingEffect.add(fields[i].trim());
+			}
+		}
+			
+		ProteingEffect.add(_ProteingEffect);
+	}
+	public void setPOS(String _POS){
+		POS = Integer.parseInt(_POS);
+	}
+	public int getPOS(){
+		return POS;
+	}
+	public void setGenePOS(String _GenePOS){
+		GenePOS = Integer.parseInt(_GenePOS);
+	}
+	public void setREF(String _REF){
+		REF = (_REF);
+	}
+	public void set_rsID(String _rsID){
+		REF = (_rsID);
+	}
+	public void addALT(String _ALT){
+		ALTs.add(_ALT);
+	}
+	public ArrayList<String> getALTs(){
+		return ALTs;
+	}
+
+
+
 
 
   @Override
@@ -24,6 +92,6 @@ public class Variant implements Comparable<Variant> {
     if (rez != 0) {
       return rez;
     }
-    return ObjectUtils.compare(GENE, o.GENE);
+    return ObjectUtils.compare(GeneName, o.GeneName);
   }
 }
