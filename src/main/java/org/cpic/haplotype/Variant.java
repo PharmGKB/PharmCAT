0954f8a479a1edb7b420808123d9a88c7881ad50
nate <nate@nate-Latitude-E6510>
2016-04-12 11:05:00 -0700

    New Variant Object

---
+++ src/main/java/org/cpic/haplotype/Variant.java
@@ -0,0 +1,16 @@
+package org.cpic.haplotype;
+
+public class Variant {
+	String CHROM;
+	int POS;
+	String cDNA;
+	String ProteingEffect;
+	String [] ALTs;
+	String REF;
+	String rsID;
+	String GENE;
+	
+	
+	
+
+}
