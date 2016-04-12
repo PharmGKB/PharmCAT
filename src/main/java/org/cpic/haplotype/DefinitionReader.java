9dacf069a6b945fbb4b26915ab8a1844098dca30
nate <nate@nate-Latitude-E6510>
2016-04-12 15:34:56 -0700

    Updates

--- src/main/java/org/cpic/haplotype/DefinitionReader.java
+++ src/main/java/org/cpic/haplotype/DefinitionReader.java
@@ -2,12 +2,17 @@ package org.cpic.haplotype;
 
 import com.google.common.base.Preconditions;
 import com.google.common.collect.Multimap;
+import com.google.common.collect.TreeMultimap;
 
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.Set;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 /**
  * Read in haplotype definition files.
@@ -15,7 +20,9 @@ import java.nio.file.Paths;
  * @author Mark Woon
  */
 public class DefinitionReader {
-  private Multimap<String, Variant> m_haplotypePositions;
+  private TreeMultimap<String, Variant> m_haplotypePositions;
+  
+  private Set<TSVfile> s_files;
 
 
   public Multimap<String, Variant> getHaplotypePositions() {
@@ -34,21 +41,75 @@ public class DefinitionReader {
       readFile(path);
     }
   }
+  
+  private int getStartPOS(String _HGVSg){
+	  
+	  Pattern p = Pattern.compile("\\d+");
+	  Matcher m = p.matcher(_HGVSg);
+	  
+	  return Integer.parseInt(m.group(1));
+	  
+  }
+  
+  
 
 
   private void readFile(Path file)  {
 
     Preconditions.checkArgument(Files.isRegularFile(file));
     System.out.println(file);
+    
+    TSVfile inProccessFile = new TSVfile(file.toString());
+    ArrayList<Variant> variants = new ArrayList<Variant>();
+    
     try (BufferedReader bufferedReader = Files.newBufferedReader(file)) {
+    	
+        String line = null;
+   	
+    	while((line = bufferedReader.readLine()) != null) {
+            
+            String[] fields = line.trim().split("\t");
+            for (int i = 0; i < fields.length; i++){
+            	fields[i] = fields[i].trim();
+            }
+            
+            if (fields[0].equals("FormatVersion")){
+            	inProccessFile.setFormatVersion(fields[1]);
+            }
+            else if (fields[0].equals("Gene")){
+            	inProccessFile.setFormatVersion(fields[1]);
+            }
+            else if (fields[0].equals("ContentVersion")){
+            	//inProccessFile.setFormatVersion(fields[1]);
+            }
+            else if (fields[0].equals("GenomeBuild")){
+            	//inProccessFile.setFormatVersion(fields[1]);
+            }
+            else if (fields[0].equals("Chomosome")){
+            	inProccessFile.setChromosomeID(fields[1]);
+            	inProccessFile.setChromosome(fields[2]);
+            }
+            else if (fields[0].equals("Protein")){
+            	//inProccessFile.setFormatVersion(fields[1]);
+            }
+            else if (fields[0].equals("cDNAChange")){
+            	for (int i = 4; i < fields.length; i++){
+                	Variant newVariant = new Variant(inProccessFile.getChromosome(),inProccessFile.getGeneName(),fields[i]);
+                	variants.add(newVariant);
+                }
+            }
+            
+            
+        } 
+    	
+    	s_files.add(inProccessFile);
     	
     	
     } catch (Exception ex) {
     	throw new RuntimeException(ex);
     }
   }
-
-
+  
   public static void main(String[] args) {
 
     try {
