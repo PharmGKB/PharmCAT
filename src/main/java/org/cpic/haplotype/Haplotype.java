1d2662c1709c0f2751b3c475ce312179d04de63c
nate <nate@nate-Latitude-E6510>
2016-04-13 10:20:13 -0700

    Initialize, get,set,add

--- src/main/java/org/cpic/haplotype/Haplotype.java
+++ src/main/java/org/cpic/haplotype/Haplotype.java
@@ -7,12 +7,49 @@ import org.cpic.util.HaplotypeNameComparator;
 
 public class Haplotype implements Comparable<Haplotype> {
 
-	ArrayList <Variant> variants;
-	String AlleleID;
-	String CommonName;
-	String FunctionStatus;
-	ArrayList <String> NormalAlleles;
-	ArrayList <String>  EffectAlleles;
+	private ArrayList <Variant> Variants = new ArrayList<>();
+	private String AlleleID;
+	private String CommonName;
+	private String FunctionStatus;
+	private ArrayList <String> NormalAlleles = new ArrayList<>();
+	private ArrayList <String>  EffectAlleles = new ArrayList<>();
+	
+	public void addVariant(Variant _Variant){
+		Variants.add(_Variant);
+	}
+	public ArrayList<Variant> getVaraints(){
+		return Variants;
+	}
+	public void setAlleleID(String _AlleleID){
+		AlleleID = _AlleleID;
+	}
+	public String getAlleleID(){
+		return AlleleID;
+	}
+	public void setCommonName(String _CommonName){
+		CommonName = _CommonName;
+	}
+	public String getCommonName(){
+		return CommonName;
+	}
+	public void setFunctionStatus(String _FunctionStatus){
+		FunctionStatus = _FunctionStatus;
+	}
+	public String getFunctionStatus(){
+		return FunctionStatus;
+	}
+	public void addNormalAlleles(String _NormalAlleles){
+		NormalAlleles.add(_NormalAlleles);
+	}
+	public ArrayList<String> getNormalAlleles(){
+		return NormalAlleles;
+	}
+	public void addEffectAlleles(String _EffectAlleles){
+		EffectAlleles.add(_EffectAlleles);
+	}
+	public ArrayList<String> getEffectAlleles(){
+		return EffectAlleles;
+	}
 
 
   @Override
