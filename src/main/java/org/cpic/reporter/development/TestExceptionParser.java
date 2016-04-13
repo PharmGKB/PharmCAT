03ede7a591b7e79666fec7970ed6d8b160021b3a
lester <lester@pharmgkb.org>
2016-04-13 15:29:38 -0700

    CPIC exception json and test added

---
+++ src/main/java/org/cpic/reporter/development/TestExceptionParser.java
@@ -0,0 +1,7 @@
+package org.cpic.reporter.development;
+
+/**
+ * Created by lester on 4/13/16.
+ */
+public class TestExceptionParser {
+}
---
+++ src/main/java/org/cpic/reporter/model/CpicExceptionList.java
@@ -0,0 +1,7 @@
+package org.cpic.reporter.model;
+
+/**
+ * Created by lester on 4/13/16.
+ */
+public class CpicExceptionList {
+}
