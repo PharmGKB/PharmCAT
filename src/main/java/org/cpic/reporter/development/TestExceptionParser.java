e9bada17e3f1466a6682a58f333dcc0968c5266d
lester <lester@pharmgkb.org>
2016-04-13 16:15:47 -0700

    Updated CPIC exception files

--- src/main/java/org/cpic/reporter/development/TestExceptionParser.java
+++ src/main/java/org/cpic/reporter/development/TestExceptionParser.java
@@ -1,7 +1,43 @@
 package org.cpic.reporter.development;
 
+import com.google.gson.Gson;
+import org.cpic.reporter.model.CpicException;
+import org.cpic.reporter.model.CpicExceptionList;
+
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+
+
+
 /**
  * Created by lester on 4/13/16.
  */
 public class TestExceptionParser {
+  public static void main(String[] args) throws Exception {
+    BufferedReader br = null;
+    try {
+      br = new BufferedReader(new FileReader("resources/cpic_exceptions/exceptions.json"));
+    } catch (FileNotFoundException e) {
+      // TODO Auto-generated catch block
+      e.printStackTrace();
+    }
+
+    Gson gson = new Gson();
+
+    CpicExceptionList cpicExceptions = gson.fromJson(br, CpicExceptionList.class);
+    System.out.println(cpicExceptions.getRules()  );
+
+    for (CpicException rule : cpicExceptions.getRules()) {
+      System.out.println(rule.getGene());
+      System.out.println("\t" + rule.getException_type());
+      System.out.println("\t" + rule.getMatches());
+      System.out.println("\t" + rule.getMessage());
+    }
+
+
+  }
+
+
 }
+
