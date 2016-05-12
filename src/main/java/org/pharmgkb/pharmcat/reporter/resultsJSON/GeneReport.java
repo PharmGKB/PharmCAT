db4b1d665c8b8a748bb4c6832a9c11036caa6493
Ryan Whaley <whaleyr@pharmgkb.org>
2016-05-12 14:32:06 -0700

    Add javadoc to GeneReport

--- src/main/java/org/pharmgkb/pharmcat/reporter/resultsJSON/GeneReport.java
+++ src/main/java/org/pharmgkb/pharmcat/reporter/resultsJSON/GeneReport.java
@@ -11,6 +11,9 @@ import org.pharmgkb.pharmcat.haplotype.model.Variant;
 import org.pharmgkb.pharmcat.reporter.model.CPICException;
 
 
+/**
+ * This class is used to help collect Gene-related data for later reporting
+ */
 public class GeneReport {
 
   private String m_gene;
@@ -18,6 +21,10 @@ public class GeneReport {
   private SortedSet<Variant> m_variants = new TreeSet<>();
   private List<CPICException> m_exceptList = new ArrayList<>();
 
+  /**
+   * public constructor
+   * @param call a {@link GeneCall} that has been made by the Haplotyper
+   */
   public GeneReport(GeneCall call) {
     m_gene = call.getGene();
     m_diplotypes = call.getDiplotypes().stream()
@@ -26,14 +33,23 @@ public class GeneReport {
     m_variants.addAll(call.getVariants());
   }
 
+  /**
+   * Diplotypes that have been called for this gene
+   */
   public Set<String> getDips(){
     return m_diplotypes;
   }
 
+  /**
+   * The gene symbol for this gene
+   */
   public String getGene(){
     return m_gene;
   }
 
+  /**
+   * The list of exceptions that apply to this gene
+   */
   public List<CPICException> getExceptionList(){
     return m_exceptList;
   }
@@ -42,6 +58,9 @@ public class GeneReport {
     m_exceptList.add(except);
   }
 
+  /**
+   * The variants that constitute this gene
+   */
   public SortedSet<Variant> getVariants() {
     return m_variants;
   }
