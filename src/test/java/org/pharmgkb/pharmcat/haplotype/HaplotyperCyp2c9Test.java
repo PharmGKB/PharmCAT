32d00fde9fa78e5ad066b358ac35c0b86608a4ed
lester <lester@pharmgkb.org>
2016-05-12 18:29:46 -0700

    CYP2C9 tests - now all work.

--- src/test/java/org/pharmgkb/pharmcat/haplotype/HaplotyperCyp2c9Test.java
+++ src/test/java/org/pharmgkb/pharmcat/haplotype/HaplotyperCyp2c9Test.java
@@ -48,19 +48,6 @@ public class HaplotyperCyp2c9Test {
   public void cyp32c9s2s24() throws Exception {
     // Test *2/*24, but also matches *1/*24
 
-    /*
-    TODO - fails.  But says it doesn't!:
-
-    *1/*24 (108), *2/*24 (108)
-    Printing to /Users/lester/IdeaProjects/cpic-annotator/build/resources/test/org/pharmgkb/pharmcat/haplotype/cyp2c9/s2s24.html
-    Expected: [*1/*24, *2/*24]
-    Got:      [*1/*24, *2/*24]
-
-    java.lang.AssertionError: Did not get expected matches
-
-     */
-
-
     Path vcfFile = TestUtil.getFile("org/pharmgkb/pharmcat/haplotype/cyp2c9/s2s24.vcf");
     List<DiplotypeMatch> matches = HaplotyperTest.testCallHaplotype(m_tsvFile, vcfFile);
 
@@ -69,21 +56,36 @@ public class HaplotyperCyp2c9Test {
   }
 
   @Test
-  public void cyp32c9s2s24hom() throws Exception {
-    // Test *2/*24 with hom at rs749060448
+  public void cyp32c9s2s24Only() throws Exception {
+    // Test *2/*24, but also matches *1/*24
 
-    /* TODO - Lester: Fails. Micelle expected *2/*24. We are checking logic on our side now.
-
-    *24/*24 (108)
-    Printing to /Users/lester/IdeaProjects/cpic-annotator/build/resources/test/org/pharmgkb/pharmcat/haplotype/cyp2c9/s2s24hom.html
-    Expected: [*2/*24]
-    Got:      [*24/*24]
-     */
-
-    Path vcfFile = TestUtil.getFile("org/pharmgkb/pharmcat/haplotype/cyp2c9/s2s24hom.vcf");
-    List<DiplotypeMatch> matches = HaplotyperTest.testCallHaplotype(m_tsvFile, vcfFile, true, false, true);
+    Path vcfFile = TestUtil.getFile("org/pharmgkb/pharmcat/haplotype/cyp2c9/s2s24only.vcf");
+    List<DiplotypeMatch> matches = HaplotyperTest.testCallHaplotype(m_tsvFile, vcfFile);
 
     List<String> expectedMatches = Lists.newArrayList("*2/*24");
     TestUtil.assertDiplotypePairs(expectedMatches, matches);
   }
+
+  @Test
+  public void cyp32c9s24s24() throws Exception {
+    // Test *24/*24 with hom at rs749060448
+
+    Path vcfFile = TestUtil.getFile("org/pharmgkb/pharmcat/haplotype/cyp2c9/s24s24.vcf");
+    List<DiplotypeMatch> matches = HaplotyperTest.testCallHaplotype(m_tsvFile, vcfFile, true, false, true);
+
+    List<String> expectedMatches = Lists.newArrayList("*24/*24");
+    TestUtil.assertDiplotypePairs(expectedMatches, matches);
+  }
+
+  @Test
+  public void cyp32c9s2s25() throws Exception {
+    // Test *2/*25
+
+    Path vcfFile = TestUtil.getFile("org/pharmgkb/pharmcat/haplotype/cyp2c9/s2s25.vcf");
+    List<DiplotypeMatch> matches = HaplotyperTest.testCallHaplotype(m_tsvFile, vcfFile, true, false, true);
+
+    List<String> expectedMatches = Lists.newArrayList("*2/*25");
+    TestUtil.assertDiplotypePairs(expectedMatches, matches);
+  }
+
 }
