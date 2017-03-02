9f60fbb85ecd03c4166fd541b91efc84265879b1
Ryan Whaley <whaleyr@pharmgkb.org>
2017-03-02 11:01:39 -0800

    Remove redundant gene diplotype data.

--- src/main/java/org/pharmgkb/pharmcat/reporter/ReportContext.java
+++ src/main/java/org/pharmgkb/pharmcat/reporter/ReportContext.java
@@ -7,8 +7,6 @@ import java.util.function.Function;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 import javax.annotation.Nonnull;
-import com.google.common.collect.Multimap;
-import com.google.common.collect.TreeMultimap;
 import org.pharmgkb.pharmcat.definition.PhenotypeMap;
 import org.pharmgkb.pharmcat.haplotype.model.GeneCall;
 import org.pharmgkb.pharmcat.reporter.model.AstrolabeCall;
@@ -36,7 +34,6 @@ import org.pharmgkb.pharmcat.reporter.model.result.GuidelineReport;
 public class ReportContext {
 
   private List<GeneCall> m_calls;
-  private Multimap<String, String> m_sampleGeneToDiplotypeMap = TreeMultimap.create();
   private Set<GeneReport> m_geneReports = new TreeSet<>();
   private List<GuidelineReport> m_interactionList;
   private PhenotypeMap m_phenotypeMap = new PhenotypeMap();
@@ -88,11 +85,7 @@ public class ReportContext {
           .reduce((r1,r2) -> { throw new RuntimeException("Didn't expect more than one report"); })
           .orElseThrow(IllegalStateException::new);
       geneReport.setCallData(call, m_phenotypeMap);
-
-      m_sampleGeneToDiplotypeMap.removeAll(call.getGene());
-      m_sampleGeneToDiplotypeMap.putAll(call.getGene(), geneReport.getDips().stream().map(d -> geneReport.getGene()+":"+d).collect(Collectors.toList()));
     }
-    fixCyp2c19();
   }
 
   /**
@@ -106,30 +99,11 @@ public class ReportContext {
           .reduce((r1,r2) -> { throw new RuntimeException("Didn't expect more than one report"); })
           .orElseThrow(IllegalStateException::new);
       geneReport.setAstrolabeData(astrolabeCall, m_phenotypeMap);
-
-      m_sampleGeneToDiplotypeMap.removeAll(astrolabeCall.getGene());
-      m_sampleGeneToDiplotypeMap.putAll(astrolabeCall.getGene(), geneReport.getDips().stream().map(d -> geneReport.getGene()+":"+d).collect(Collectors.toList()));
     }
   }
 
   private boolean isCalled(String gene) {
     return m_geneReports.stream().filter(r -> r.getGene().equals(gene)).allMatch(GeneReport::isCalled);
-  }
-
-  /**
-   * Substitutes "*4" for "*4A" or "*4B" in the map of called diplotypes
-   *
-   * This is a temporary fix while the allele definitions for CYP2C19 don't match what's been annotated by CPIC
-   *
-   */
-  private void fixCyp2c19() {
-    if (m_sampleGeneToDiplotypeMap.keySet().contains("CYP2C19")) {
-      List<String> fixedDiplotypes = m_sampleGeneToDiplotypeMap.get("CYP2C19").stream()
-          .map(d -> d.replaceAll("\\*4[AB]", "*4"))
-          .collect(Collectors.toList());
-      m_sampleGeneToDiplotypeMap.removeAll("CYP2C19");
-      m_sampleGeneToDiplotypeMap.putAll("CYP2C19", fixedDiplotypes);
-    }
   }
 
   /**
@@ -143,14 +117,15 @@ public class ReportContext {
 
     for(GuidelineReport guideline : m_interactionList) {
       boolean reportable = guideline.getRelatedGeneSymbols().stream()
-          .allMatch(this::isCalled);
+          .anyMatch(this::isCalled);
 
       guideline.setReportable(reportable);
 
+      guideline.getRelatedGeneSymbols().stream()
+          .filter(g -> !isCalled(g))
+          .forEach(guideline::addUncalledGene);
+
       if (!reportable) {
-        guideline.getRelatedGeneSymbols().stream()
-            .filter(g -> !isCalled(g))
-            .forEach(guideline::addUncalledGene);
         continue;
       }
 
@@ -182,14 +157,14 @@ public class ReportContext {
 
   private Set<String> makeCalledGenotypes(GuidelineReport guidelineReport, String symbol, Set<String> results) {
     if (results.size() == 0) {
-      return m_sampleGeneToDiplotypeMap.get(symbol).stream()
-          .map(guidelineReport::translateToPhenotype)
+      return getGeneReport(symbol).getDips().stream()
+          .map(d -> guidelineReport.translateToPhenotype(symbol + ":" + d))
           .collect(Collectors.toSet());
     }
     else {
       Set<String> newResults = new TreeSet<>();
       for (String geno1 : results) {
-        m_sampleGeneToDiplotypeMap.get(symbol).stream().map(guidelineReport::translateToPhenotype).forEach(
+        getGeneReport(symbol).getDips().stream().map(d -> guidelineReport.translateToPhenotype(symbol + ":" + d)).forEach(
             geno2 -> {
               Set<String> genos = new TreeSet<>();
               genos.add(geno1);
@@ -214,12 +189,5 @@ public class ReportContext {
     return m_geneReports.stream().filter(r -> r.getGene().equals(geneSymbol))
         .reduce((r1,r2) -> { throw new RuntimeException("Duplicate gene reports found"); })
         .orElseThrow(RuntimeException::new);
-  }
-
-  public Set<String> makeGenePhenotypes(@Nonnull String geneSymbol) {
-    return m_interactionList.stream()
-        .filter(g -> g.getRelatedGeneSymbols().contains(geneSymbol) && g.getMatchingGroups() != null)
-        .flatMap(g -> g.getMatchingGroups().stream())
-        .map(Group::getName).collect(Collectors.toSet());
   }
 }
