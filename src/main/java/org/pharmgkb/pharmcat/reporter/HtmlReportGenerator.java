6c567b8789c4e9b8527dfd2f3beee4be97707886
Ryan Whaley <whaleyr@pharmgkb.org>
2018-04-25 17:04:18 -0700

    Cleanup unused report
    * don't generate a separate disclaimer report anymore

--- src/main/java/org/pharmgkb/pharmcat/reporter/HtmlReportGenerator.java
+++ src/main/java/org/pharmgkb/pharmcat/reporter/HtmlReportGenerator.java
@@ -5,7 +5,6 @@ import java.io.IOException;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Path;
-import java.util.Collections;
 import java.util.Map;
 import javax.annotation.Nonnull;
 import com.github.jknack.handlebars.Handlebars;
@@ -22,7 +21,6 @@ import org.pharmgkb.pharmcat.reporter.handlebars.ReportHelpers;
  */
 public class HtmlReportGenerator {
   private static final String FINAL_REPORT      = "report";
-  private static final String DISCLAIMER_REPORT = "disclaimerReport";
   private static final String sf_templatePrefix = "/org/pharmgkb/pharmcat/reporter";
 
   /**
@@ -31,29 +29,14 @@ public class HtmlReportGenerator {
    * @param filePath the path to write the report to
    */
   public static void writeFinalReport(@Nonnull Map<String,Object> data, @Nonnull Path filePath) throws IOException {
-    writeReport(FINAL_REPORT, data, filePath);
-  }
+    Handlebars handlebars = new Handlebars(new ClassPathTemplateLoader(sf_templatePrefix));
+    StringHelpers.register(handlebars);
+    handlebars.registerHelpers(ReportHelpers.class);
+    Template template = handlebars.compile(FINAL_REPORT);
 
-  /**
-   * Generate a disclaimer report to review the disclaimer text
-   * @param filePath the path to write the report to
-   */
-  public static void writeDisclaimerReport(@Nonnull Path filePath) throws IOException {
-    writeReport(DISCLAIMER_REPORT, Collections.emptyMap(), filePath);
-  }
-
-  private static void writeReport(@Nonnull String reportName, @Nonnull Map<String,Object> data, @Nonnull Path filePath) throws IOException {
-    Template template = initTemplate(reportName);
     String html = template.apply(data);
     try (BufferedWriter writer = Files.newBufferedWriter(filePath, StandardCharsets.UTF_8)) {
       writer.write(html);
     }
-  }
-
-  private static Template initTemplate(String templateName) throws IOException {
-    Handlebars handlebars = new Handlebars(new ClassPathTemplateLoader(sf_templatePrefix));
-    StringHelpers.register(handlebars);
-    handlebars.registerHelpers(ReportHelpers.class);
-    return handlebars.compile(templateName);
   }
 }
