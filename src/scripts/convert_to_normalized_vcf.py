6d0e7f4f6faa805221c3c8c4a8ac1d144261084d
Binglan Li <bl.victoria.li@gmail.com>
2021-02-09 12:04:41 -0800

    add descriptions for the bcftools and tabix flags; remove the '--extract_regions' flag from convert_to_normalized_vcf.py; replace the '--input_vcf' with the '--normalized_vcf' flag for the convert_to_pharmcat_vcf.py

--- src/scripts/convert_to_normalized_vcf.py
+++ src/scripts/convert_to_normalized_vcf.py
@@ -6,6 +6,12 @@ import os
 import subprocess
 
 def run(args):
+    ## this section invokes external tabix and bcftools to normalize an input VCF file
+    ## tabix commands are exclusively "tabix -p vcf <input_vcf>", which generates an index file (.tbi) for an input file (<input_file>) whose file type is specified by "-p vcf". The .tabi is, by default, output to the current working directory. 
+    ## "bcftools <common_options> <input_vcf>". "-O" (capitalized letter O) specifies the output type as compressed VCF (z). "-o" writes to a file rather than to default standard output.
+    ## "bcftools annotate <options> <input_vcf>" annotates and converts the VCF column info. "--rename-chrs <chr_name_mapping_file>" convert chromosome names from the old ones to the new ones.
+    ## "bcftools norm <options> <input_vcf>" normalizes the input VCF file. "-m+" join biallelic sites into multiallelic records. "-f" reference genome sequence in fasta format. "--check-ref" when incorrect or missing REF allele is encountered: exit (e), warn (w), exclude (x), or set/fix (s) bad sites.
+
     # assign the current working directory as the output path if not specified
     output_folder = os.getcwd() if not args.output_folder else args.output_folder
     # organize args
@@ -13,39 +19,34 @@ def run(args):
     tabix_executable_path = args.path_to_tabix if args.path_to_tabix else "tabix"
     bcftools_executable_path = args.path_to_bcftools if args.path_to_bcftools else "bcftools"
 
-    # validate the input arguments, write another function
+    # (function pending) validate the input arguments
     #validate(args)
 
-    # if the input VCF file is not index (.tbi doesn't exist), index the file using tabix
+    # if the input VCF file is not indexed (.tbi doesn't exist), index the file using tabix
     if not os.path.exists(args.input_vcf + ".tbi"):
-        saved_wd = os.getcwd()
         input_folder = os.path.split(args.input_vcf)[0]
         subprocess.run([tabix_executable_path, "-p", "vcf", args.input_vcf], cwd = input_folder)
-        os.chdir(saved_wd)
 
     # rename chromosomes
-    output_temp = os.path.join(output_folder, args.output_prefix + ".temp_chr_renamed.vcf.gz")
-    print(" ".join([bcftools_executable_path, "annotate", "--rename-chrs", args.rename_chrs, "-Oz", "-o", output_temp, args.input_vcf]))
-    subprocess.run([bcftools_executable_path, "annotate", "--rename-chrs", args.rename_chrs, "-Oz", "-o", output_temp, args.input_vcf])
-    #saved_wd = os.getcwd()
-    subprocess.run([tabix_executable_path, "-p", "vcf", output_temp], cwd = output_folder)
-    #os.chdir(saved_wd)
+    input_renamed_chr = os.path.join(output_folder, args.output_prefix + ".temp_chr_renamed.vcf.gz")
+    # explain bcftools flags (find a place in the script to explain it)
+    subprocess.run([bcftools_executable_path, "annotate", "--rename-chrs", args.rename_chrs, "-Oz", "-o", input_renamed_chr, args.input_vcf])
+    subprocess.run([tabix_executable_path, "-p", "vcf", input_renamed_chr], cwd = output_folder)
 
     # normalize the input VCF
-    if args.extract_regions:
-        subprocess.run([bcftools_executable_path, "norm", "-m+", "-Oz", "-o", output_full_name, "-R", args.extract_regions, "-f", args.ref_seq, output_temp])
-    else:
-        subprocess.run([bcftools_executable_path, "norm", "-m+", "-Oz", "-o", output_full_name, "-f", args.ref_seq, output_temp])
+    # modify this part to comply to the PharmCAT VCF requirements and PharmCAT only
+    # explain bcftools flags
+    subprocess.run([bcftools_executable_path, "norm", "--check-ref", "s", "-m+", "-Oz", "-o", output_full_name, "-f", args.ref_seq, input_renamed_chr])
 
     # index the output VCF
     subprocess.run([tabix_executable_path, "-p", "vcf", output_full_name], cwd = args.output_folder)
 
     # remove temporary files
     try:
-        os.remove(output_temp)
-        os.remove(output_temp + ".tbi")
+        os.remove(input_renamed_chr)
+        os.remove(input_renamed_chr + ".tbi")
     except OSError as error_remove_tmp:
-        print("Error: %s : %s" % (output_temp, error_remove_tmp.strerror))
+        print("Error: %s : %s" % (input_renamed_chr, error_remove_tmp.strerror))
     
     
 
@@ -57,13 +58,12 @@ if __name__ == "__main__":
 
     # list arguments
     parser.add_argument("--input_vcf", help="Load a compressed VCF file.")
-    parser.add_argument("--extract_regions", help="Extract the regions from the VCF. The region file has to be tab-delimited with CHROM, POS, and optionally END-POS.")
     parser.add_argument("--ref_seq", help="Load the Human Reference Genome GRCh38/hg38 in the fasta format.")
     parser.add_argument("--rename_chrs", help="Load a chromosome rename map file")
     parser.add_argument("--path_to_bcftools", help="Load an alternative path to the executable bcftools.")
     parser.add_argument("--path_to_tabix", help="Load an alternative path to the executable tabix.")
     parser.add_argument("--output_folder", help="Directory of the output VCF, by default, current working directory.")
-    parser.add_argument("--output_prefix", help="Name of the output VCF")
+    parser.add_argument("--output_prefix", help="Prefix of the output VCF")
 
     # parse arguments
     args = parser.parse_args()
--- src/scripts/convert_to_pharmcat_vcf.py
+++ src/scripts/convert_to_pharmcat_vcf.py
@@ -6,6 +6,12 @@ import os
 import subprocess
 
 def run(args):
+    ## this section invokes external tabix and bcftools to normalize an input VCF file
+    ## tabix commands are exclusively "tabix -p vcf <input_vcf>", which generates an index file (.tbi) for an input file (<input_file>) whose file type is specified by "-p vcf". The .tabi is, by default, output to the current working directory. 
+    ## "bcftools <common_options> <input_vcf>". "-O" (capitalized letter O) specifies the output type as compressed VCF (z). "-o" writes to a file rather than to default standard output.
+    ## "bcftools annotate <options> <input_vcf>" annotates and converts the VCF column info. "--rename-chrs <chr_name_mapping_file>" convert chromosome names from the old ones to the new ones.
+    ## "bcftools norm <options> <input_vcf>" normalizes the input VCF file. "-m+" join biallelic sites into multiallelic records. "-f" reference genome sequence in fasta format. "--check-ref" when incorrect or missing REF allele is encountered: exit (e), warn (w), exclude (x), or set/fix (s) bad sites.
+
     # assign the current working directory as the output path if not specified
     output_folder = os.getcwd() if not args.output_folder else args.output_folder
     # organize args
@@ -13,22 +19,22 @@ def run(args):
     bcftools_executable_path = args.path_to_bcftools if args.path_to_bcftools else "bcftools"
     input_folder = os.path.split(args.input_vcf)[0]
 
-    # validate the input arguments, write another function
+    # (function pending) validate the input arguments
     #validate(args)
 
     # if the input VCF file is not index (.tbi doesn't exist), index the file using tabix
     if not os.path.exists(args.input_vcf + ".tbi"):
-        saved_wd = os.getcwd()
         subprocess.run([tabix_executable_path, "-p", "vcf", args.input_vcf], cwd = input_folder)
-        os.chdir(saved_wd)
 
     # obtain list of samples
+    # explain bcftools flags
     input_vcf_sample_list = subprocess.check_output([bcftools_executable_path, "query", "-l", args.input_vcf], universal_newlines=True, cwd = input_folder).split('\n')
     input_vcf_sample_list.pop()
 
     # generate PharmCAT-ready single-sample VCF file(s)
     for input_vcf_single_sample in input_vcf_sample_list:
         output_full_name = os.path.join(output_folder, args.output_prefix + "." + input_vcf_single_sample + ".vcf.gz")
+        # explain bcftools flags
         bcftools_command = [bcftools_executable_path, "view", "-Oz", "-o", output_full_name]
         bcftools_command.extend(["-s", input_vcf_single_sample, args.input_vcf]) if args.ref_pgx_vcf else bcftools_command.append(args.input_vcf) 
 
@@ -37,6 +43,7 @@ def run(args):
     # output a VCF file of missing PGx positions
     if args.ref_pgx_vcf:
         output_missing_pos_full_name = os.path.join(output_folder, args.output_prefix + ".missing_pgx_var.vcf.gz")
+        # explain bcftools flags
         bcftools_command = [bcftools_executable_path, "isec", "-c", "indels", "-w1", "-Oz", "-o", output_missing_pos_full_name, "-C", args.ref_pgx_vcf, args.input_vcf]
         subprocess.run(bcftools_command, cwd = args.output_folder)
 
@@ -48,12 +55,12 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='convert_to_normalized_vcf.py :  Will normalize the input VCF file, as well as reconstruct multi-allelic variants')
 
     # list arguments
-    parser.add_argument("--input_vcf", help="Load a compressed, normalized VCF file.")
+    parser.add_argument("--normalized_vcf", help="Load a compressed, normalized VCF file.")
     parser.add_argument("--ref_pgx_vcf", help="Load a VCF file of PGx variants. This file is available from the PharmCAT GitHub release.")
     parser.add_argument("--path_to_bcftools", help="Load an alternative path to the executable bcftools.")
     parser.add_argument("--path_to_tabix", help="Load an alternative path to the executable tabix.")
     parser.add_argument("--output_folder", help="Directory of the output VCF, by default, current working directory.")
-    parser.add_argument("--output_prefix", help="Name of the output VCF")
+    parser.add_argument("--output_prefix", help="Prefix of the output VCF")
 
     # parse arguments
     args = parser.parse_args()
