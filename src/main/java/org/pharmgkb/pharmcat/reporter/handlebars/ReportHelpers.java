4e891252dc6e7b9c20b28b9cb5a65d23ae74a2f3
Ryan Whaley <whaleyr@pharmgkb.org>
2021-02-22 11:26:46 -0800

    fix: remove styling for Rx change in report

--- src/main/java/org/pharmgkb/pharmcat/reporter/handlebars/ReportHelpers.java
+++ src/main/java/org/pharmgkb/pharmcat/reporter/handlebars/ReportHelpers.java
@@ -12,31 +12,15 @@ import org.pharmgkb.pharmcat.reporter.model.VariantReport;
  */
 public class ReportHelpers {
 
-  private static final String sf_drugNameTemplate = "<div class=\"drugName\"><div class=\"%s\">%s <a href=\"#%s\">%s</a></div></div>";
+  private static final String sf_drugNameTemplate = "<div class=\"drugName\"><div class=\"%s\">%s<a href=\"#%s\">%s</a></div></div>";
   private static final String sf_variantAlleleTemplate = "<td class=\"%s\">%s%s</td>";
 
-  private static final String sf_iconHighlight = "<i class=\"fa fa-square highlight\" aria-hidden=\"true\" title=\"Consult guideline\"></i>";
-  private static final String sf_iconPossibly = "<i class=\"fa fa-exclamation-triangle rxPossibly\" aria-hidden=\"true\" title=\"Possible Rx change\"></i>";
-  private static final String sf_iconChange = "<i class=\"fa fa-times rxChange\" aria-hidden=\"true\" title=\"Rx change\"></i>";
-  private static final String sf_iconNormal = "<i class=\"fa fa-circle normal\" aria-hidden=\"true\" title=\"No Rx change\"></i>";
+  private static final String sf_iconHighlight = "<i class=\"fa fa-square highlight\" aria-hidden=\"true\" title=\"Consult guideline\"></i> ";
 
   public static String drug(DrugLink drug) {
-    String cn;
-    String icon;
+    String cn = "";
+    String icon = "";
 
-    cn = " " + drug.getRxClass();
-
-    switch (drug.getRxClass()) {
-      case "rxChange":
-        icon = sf_iconChange;
-        break;
-      case "rxPossibly":
-        icon = sf_iconPossibly;
-        break;
-      default:
-        icon = sf_iconNormal;
-        break;
-    }
     if (drug.isHighlighted()) {
       cn = " highlightDrug";
       icon = sf_iconHighlight;
