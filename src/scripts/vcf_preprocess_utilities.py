62427e9420a0ffd903cc8481a4b9a47a99aba08b
Binglan Li <bl.victoria.li@gmail.com>
2021-06-22 13:39:26 -0700

    fix: file path split

--- src/scripts/vcf_preprocess_utilities.py
+++ src/scripts/vcf_preprocess_utilities.py
@@ -84,7 +84,7 @@ def tabix_index_vcf(tabix_executable_path, vcf_path):
     '''
     
     try:
-        subprocess.run([tabix_executable_path, '-p', 'vcf', vcf_path], cwd = os.path.split(vcf_path)[0])
+        subprocess.run([tabix_executable_path, '-p', 'vcf', vcf_path], cwd = os.path.split(os.path.abspath(vcf_path))[0])
     except Exception as e:
         print('Error: cannot index the file: %s' %vcf_path)
         traceback.print_exception(type(e), e, e.__traceback__)
