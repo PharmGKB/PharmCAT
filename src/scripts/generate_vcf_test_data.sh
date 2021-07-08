671fa011868754a9e84cd17c612c988a091a2e5b
Mark Woon <markwoon@gmail.com>
2021-07-07 17:15:33 -0700

    test: update to use autogenerated vcfs with missing positions

--- src/scripts/generate_vcf_test_data.sh
+++ src/scripts/generate_vcf_test_data.sh
@@ -4,6 +4,28 @@
 # This script expects to be called within the PharmCAT repository (all paths are hard coded).
 #
 #
+set -e
+set -u
+set -o pipefail
+
+
+MISSING="false"
+
+while getopts 'm' OPTION; do
+  case "$OPTION" in
+    m)
+      MISSING="true"
+      ;;
+
+    ?)
+      echo "script usage: $(basename $0) [-m]" >&2
+      exit 1
+      ;;
+  esac
+done
+shift "$(($OPTIND -1))"
+
+
 
 # cd to location of script
 cd $(dirname $0)
@@ -20,7 +42,11 @@ for file in "$DEFINITION_DIR"/*; do
     if [[ $gene != "CYP2D6" ]]; then
       echo "$gene"
       mkdir -p ${OUTPUT_DIR}/${gene}
-      ./test_gen.py $file ${VCF_FILE} ${OUTPUT_DIR}/${gene}
+      if [[ $MISSING == "false" ]]; then
+        ./test_gen.py $file ${VCF_FILE} ${OUTPUT_DIR}/${gene}
+      else
+        ./test_gen_missing.py $file ${VCF_FILE} ${OUTPUT_DIR}/${gene}
+      fi
       echo ""
       echo ""
     fi
