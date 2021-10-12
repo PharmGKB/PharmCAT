da2a456cd2b7707f55b3c91af99698ccca3c9676
Binglan Li <bl.victoria.li@gmail.com>
2021-10-12 10:32:45 -0700

    fix: normalize "chrom" field to chrM for mitochondria

--- src/scripts/preprocessor/vcf_preprocess_utilities.py
+++ src/scripts/preprocessor/vcf_preprocess_utilities.py
@@ -206,10 +206,10 @@ def extract_regions_from_single_file(bcftools_path, tabix_path, input_vcf, pgx_v
     elif _has_chr_string(input_vcf) == "false":
         # chromosome names
         chr_names_wo_strings = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "13", "14", "15", "16",
-                                "17", "18", "19", "20", "21", "22"]
+                                "17", "18", "19", "20", "21", "22", "X", "Y", "M", "MT", "chrMT"]
         chr_names_w_strings = ["chr1", "chr2", "chr3", "chr4", "chr5", "chr6", "chr7", "chr8", "chr9", "chr10", "chr11",
                                "chr12", "chr13", "chr14", "chr15", "chr16", "chr17", "chr18", "chr19", "chr20", "chr21",
-                               "chr22"]
+                               "chr22", "chrX", "chrY", "chrM", "chrM", "chrM"]
         # check if two chr arrays are of the same length
         if len(chr_names_wo_strings) != len(chr_names_w_strings):
             print("Error in internal chromosome mapping arrays")
