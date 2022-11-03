6fc555015cc997af018656266787133d5db9724e
Binglan Li <bl.victoria.li@gmail.com>
2022-11-03 14:58:21 -0700

    fix: bug in reference genome sequence

--- bin/referenceFasta.sh
+++ bin/referenceFasta.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 #
 # Builds reference FASTA file for use by VCF preprocessor.
+# Download from https://ftp.ncbi.nlm.nih.gov/genomes/all/GCF/000/001/405/GCF_000001405.39_GRCh38.p13/GRCh38_major_release_seqs_for_alignment_pipelines/
+# The no_alt_analysis_set contains the sequences, in FASTA format, of the chromosomes, mitochondrial genome, unlocalized scaffolds, and unplaced scaffolds.
+# The definition line has a UCSC-style sequence identifier and contains metadata in a series of space-separated tag-value pairs.
+# Soft-masking (low complexity sequence aka repetitive regions) is converted to uppercases in reference genome sequences for alignment pipelines.
 #
 set -e
 set -u
@@ -11,23 +15,20 @@ rm -f GRCh38_reference_fasta.tar reference.fna.bgz reference.fna.bgz.fai referen
 
 echo ""
 echo "Downloading from NCBI..."
-curl -#fSL https://ftp.ncbi.nlm.nih.gov/genomes/all/GCF/000/001/405/GCF_000001405.39_GRCh38.p13/GCF_000001405.39_GRCh38.p13_genomic.fna.gz -o genomic.fna.gz
+curl -#fSL https://ftp.ncbi.nlm.nih.gov/genomes/all/GCF/000/001/405/GCF_000001405.39_GRCh38.p13/GRCh38_major_release_seqs_for_alignment_pipelines/GCA_000001405.15_GRCh38_full_plus_hs38d1_analysis_set.fna.gz -o genomic.fna.gz
 
 echo ""
 echo "Uncompressing..."
 gunzip genomic.fna.gz
 
-echo ""
-echo "Fixing chromosomes..."
-cat genomic.fna | sed -r 's/^>(NC.*Homo sapiens chromosome ([0-9XY]+),.*)/>chr\2 \1/g' | sed -r 's/^>(NC.*Homo sapiens mitochondrion,.*)/>chrM \1/g' > chrfix.fna
-
+# 'AS' means assembly-name. Only keep the main GRCh38 assembly sequence
 echo ""
 echo "Removing non-primary sequences"
-awk '{ if ((NR>1)&&($0~/^>/)) { printf("\n%s", $0); } else if (NR==1) { printf("%s", $0); } else { printf("\t%s", $0); } }' chrfix.fna | grep ">chr" - | tr "\t" "\n" > chrfix.short.fna
+awk '{ if ((NR>1)&&($0~/^>/)) { printf("\n%s", $0); } else if (NR==1) { printf("%s", $0); } else { printf("\t%s", $0); } }' genomic.fna | grep -v "^>chr\S*_" - | tr "\t" "\n" > genomic.short.fna
 
 echo ""
 echo "Compressing with bgzip..."
-bgzip -c chrfix.short.fna > reference.fna.bgz
+bgzip -c genomic.short.fna > reference.fna.bgz
 
 echo ""
 echo "Building index..."
