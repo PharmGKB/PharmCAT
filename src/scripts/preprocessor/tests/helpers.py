49bb3960ed24bbc0ee3f9ebb98948626da0b5008
Mark Woon <markwoon@gmail.com>
2022-11-13 16:23:49 -0800

    test(preprocessor): add helper method to split test.vcf.bgz into test1.vcf.bgz and test2.vcf.bgz

--- src/scripts/preprocessor/tests/helpers.py
+++ src/scripts/preprocessor/tests/helpers.py
@@ -1,10 +1,12 @@
 import hashlib
+import re
 import shutil
+import tempfile
 from pathlib import Path
-from typing import Optional
+from typing import Optional, List
 
 import preprocessor
-from preprocessor import download_reference_fasta_and_index
+from preprocessor import download_reference_fasta_and_index, ReportableException, bgzip_vcf
 
 
 TEST_DOWNLOAD = False
@@ -56,4 +58,58 @@ def compare_vcf_files(expected: Path, tmp_dir: Path, basename: str, sample: str,
     assert actual.is_file(), '%s not found' % actual
     assert read_vcf(expected) == read_vcf(actual), '%s mismatch' % sample
 
+
+def split_test_vcf():
+    test_vcf = test_dir / 'test.vcf.bgz'
+    test1_vcf = test_dir / 'test1.vcf.bgz'
+    test2_vcf = test_dir / 'test2.vcf.bgz'
+    with tempfile.TemporaryDirectory() as td:
+        tmp_dir: Path = Path(td)
+        tmp_vcf_gz = tmp_dir / 'test.vcf.gz'
+        shutil.copyfile(test_vcf, tmp_vcf_gz)
+
+        preprocessor.run(['gunzip', str(tmp_vcf_gz)])
+        tmp_vcf = tmp_dir / 'test.vcf'
+        assert tmp_vcf.is_file()
+
+        comments: List[str] = []
+        data1: List[str] = []
+        data2: List[str] = []
+        pattern = re.compile(r'(?:chr)?([XYM]|\d+)\s.*')
+        with open(tmp_vcf, 'r') as f:
+            for line in f:
+                line = line.rstrip()
+                if line[0] == '#':
+                    comments.append(line)
+                else:
+                    rez = pattern.match(line)
+                    if not rez:
+                        raise ReportableException('Unrecognized chromosome in: %s' % line)
+                    if rez.group(1).isnumeric() and int(rez.group(1)) <= 10:
+                        data1.append(line)
+                    else:
+                        data2.append(line)
+
+        tmp_vcf1 = tmp_dir / 'test1.vcf'
+        with open(tmp_vcf1, 'w') as f:
+            f.write('\n'.join(comments))
+            f.write('\n')
+            f.write('\n'.join(data1))
+            f.write('\n')
+        tmp_bgz1 = bgzip_vcf(tmp_vcf1)
+
+        tmp_vcf2 = tmp_dir / 'test2.vcf'
+        with open(tmp_vcf2, 'w') as f:
+            f.write('\n'.join(comments))
+            f.write('\n')
+            f.write('\n'.join(data2))
+            f.write('\n')
+        tmp_bgz2 = bgzip_vcf(tmp_vcf2)
+
+        shutil.copyfile(tmp_bgz1, test1_vcf)
+        shutil.copyfile(tmp_bgz2, test2_vcf)
+
+
+if __name__ == "__main__":
+    split_test_vcf()
 
