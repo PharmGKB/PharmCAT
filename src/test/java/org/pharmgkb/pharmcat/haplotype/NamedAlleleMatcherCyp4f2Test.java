da45ebe1b209120be9796401fe549bbd6014f16d
Mark Woon <markwoon@gmail.com>
2023-01-31 19:00:59 -0800

    test: update tests for new CYP4F2 haplotypes

--- src/test/java/org/pharmgkb/pharmcat/haplotype/NamedAlleleMatcherCyp4f2Test.java
+++ src/test/java/org/pharmgkb/pharmcat/haplotype/NamedAlleleMatcherCyp4f2Test.java
@@ -20,32 +20,22 @@ class NamedAlleleMatcherCyp4f2Test {
 
 
   @Test
-  void s2_s3(TestInfo testInfo) throws Exception {
-    Path vcfFile = new TestVcfBuilder(testInfo, "*2/*3")
+  void s1_s4(TestInfo testInfo) throws Exception {
+    Path vcfFile = new TestVcfBuilder(testInfo, "*1/*4")
         .saveFile()
         .variation("CYP4F2", "rs3093105", "C", "A")
         .variation("CYP4F2", "rs2108622", "C", "T")
         .generate();
-    assertDiplotypePairs("*2/*3", testMatchNamedAlleles(sf_definitionFile, vcfFile));
+    assertDiplotypePairs("*1/*4", testMatchNamedAlleles(sf_definitionFile, vcfFile, true));
   }
 
   @Test
-  void s2_s2s3(TestInfo testInfo) throws Exception {
-    Path vcfFile = new TestVcfBuilder(testInfo, "*2/*2+*3")
+  void s2_s4(TestInfo testInfo) throws Exception {
+    Path vcfFile = new TestVcfBuilder(testInfo, "*2/*4")
         .saveFile()
         .variation("CYP4F2", "rs3093105", "C", "C")
         .variation("CYP4F2", "rs2108622", "C", "T")
         .generate();
-    assertDiplotypePairs("*2/[*2 + *3]", testMatchNamedAlleles(sf_definitionFile, vcfFile, true, true));
-  }
-
-  @Test
-  void s2s3_s2s3(TestInfo testInfo) throws Exception {
-    Path vcfFile = new TestVcfBuilder(testInfo, "*2+*3/*2+*3")
-        .saveFile()
-        .variation("CYP4F2", "rs3093105", "C", "C")
-        .variation("CYP4F2", "rs2108622", "T", "T")
-        .generate();
-    assertDiplotypePairs("[*2 + *3]/[*2 + *3]", testMatchNamedAlleles(sf_definitionFile, vcfFile, true, true));
+    assertDiplotypePairs("*2/*4", testMatchNamedAlleles(sf_definitionFile, vcfFile, true, true));
   }
 }
