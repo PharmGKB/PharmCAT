15a39a027b0199460e54aceb46a22228f4621c70
Mark Woon <markwoon@gmail.com>
2025-02-20 01:47:13 -0800

    doc(phenotyper): improve error message

--- src/main/java/org/pharmgkb/pharmcat/phenotype/Phenotyper.java
+++ src/main/java/org/pharmgkb/pharmcat/phenotype/Phenotyper.java
@@ -178,7 +178,7 @@ public class Phenotyper {
    */
   public static Phenotyper read(Path filePath) throws IOException {
     Preconditions.checkNotNull(filePath);
-    Preconditions.checkArgument(Files.isRegularFile(filePath));
+    Preconditions.checkArgument(Files.isRegularFile(filePath), "Not a file: " + filePath);
     try (BufferedReader reader = Files.newBufferedReader(filePath)) {
       return DataSerializer.GSON.fromJson(reader, Phenotyper.class);
     }
